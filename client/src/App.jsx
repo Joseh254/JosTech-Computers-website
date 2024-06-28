9e8a9e59dadc7e4d74b24de8ef307c67d95c56c1
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-06-28 15:56:39 +0300

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
-
+import Home from "./pages/Home"
+import Header from "../Components/Header"
+import About from "./pages/About"
+import FeaturedProducts from "./pages/FeaturedProducts"
+import Contact from "./pages/Contact"
+import Footer from "../Components/Footer"
 
 function App() {
   return (
     <>
-<p>hello world</p>
+<Header/>
    
  
     </>
---
+++ client/src/pages/FeaturedProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeaturedProducts() {
+  return (
+    <div>FeaturedProducts</div>
+  )
+}
+
+export default FeaturedProducts
