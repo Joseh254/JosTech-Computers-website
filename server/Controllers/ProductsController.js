44fcbcfcce76d8338e059f05932d25ad5ea69507
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-03 11:25:19 +0300

    create controllers

---
+++ server/Controllers/ProductsController.js
@@ -0,0 +1,21 @@
+
+export function getallProducts (request,response) {
+    response.send("getting all products")
+}
+
+
+ export function getSingleProduct (request,response){
+    response.send("getting single product")
+}
+
+export function createProduct (request,response){
+    response.send("creating a product")
+}
+
+export function deleteProduct (request,response){
+    response.send("deleting a product")
+}
+
+export function updateProduct (request,response){
+    response.send("updating product")
+}
--- server/Routes/Products.route.js
+++ server/Routes/Products.route.js
@@ -1,19 +1,15 @@
-import { Router, request, response } from "express";
+import { Router} from "express";
+import {getallProducts,getSingleProduct,createProduct,deleteProduct,updateProduct} from '../Controllers/ProductsController.js'
 const router = Router();
 
-router.get("/Products",(request,response)=>{
-    response.send("getting all products")
-})
+router.get("/Products",getallProducts)
 
-router.get("/products/:id",(request,response)=>{
-    response.send("getting single product")
-})
+router.get("/products/:id", getSingleProduct)
 
-router.post("/products",(request,response)=>{
-    response.send("creating a product")
-})
-router.delete("/products/:id",(request,response)=>{
-    response.send("deleting a product")
-})
+router.post("/products", createProduct)
+
+router.delete("/products/:id", deleteProduct)
+
+router.patch("/products/:id",updateProduct)
 
 export default router
