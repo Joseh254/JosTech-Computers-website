869a6c91456a95a99e886841be958064fef43cc5
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-07-06 13:13:08 +0300

    validate confirm password

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,38 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./Signin.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Signin.css";
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebookMessenger } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaFacebookMessenger, FaGithub } from "react-icons/fa";
 import { useFormik } from 'formik';
 import { useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 function Signin() {
 
-  const [loading, setloading] = useState(false);
-  const [error, seterror] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  const handleSubmit = async (values)=>{
+  const handleSubmit = async (values) => {
     try {
-      setloading(true);
-      const createUser = await axios.post("http://localhost:3000/api/users/register", {
-          firstName: values.firstname,
-          lastName:values.lastname,
-          email:values.email,
-          password:values.password
-      }).then(res => {
-        console.log(res)
-      }).catch(error => {
-        console.log(error)
-      })
-      console.log(createUser);
-      setloading(false);
+      setLoading(true);
+      const response = await axios.post("http://localhost:3000/api/users/register", {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        email: values.email,
+        password: values.password
+      });
+      console.log(response);
+      setLoading(false);
     } catch (error) {
-      seterror(error.message);
-      setloading(false);
+      setError(error.message);
+      setLoading(false);
     }
-  }
+  };
 
   const formik = useFormik({
     initialValues: {
@@ -40,27 +35,18 @@ function Signin() {
       lastName: "",
       email: "",
       password: "",
-     
+      confirmPassword: ""
     },
     onSubmit: handleSubmit,
-    validate: function(formvalues){
+    validate: (values) => {
       let errors = {};
-      if(formvalues.firstname === "")
-        errors.firstname = "First name is required";
+      if (!values.firstName) errors.firstName = "First name is required";
+      if (!values.lastName) errors.lastName = "Last name is required";
+      if (!values.email) errors.email = "Email is required";
+      if (!values.password) errors.password = "Password is required";
+      if (values.password !== values.confirmPassword) errors.confirmPassword = "Passwords must match";
 
-      if(formvalues.lastname === "")
-        errors.lastname = "Last name is required";
-
-      if(formvalues.email === "")
-        errors.email = "Email is required";
-
-      if(formvalues.password === "")
-        errors.password = "Password is required";
-        
-      if(formvalues.confirm_password === "")
-        errors.confirm_password = "Please confirm your password";
-
-      return errors; 
+      return errors;
     }
   });
 
@@ -68,46 +54,48 @@ function Signin() {
     <section className='signinpage'>
       <div className='signinform'>
         <h1>Create An Account</h1>
-        <p>Already have an Account?<Link to="/Login">Log in to your account</Link></p>
+        <p>Already have an Account? <Link to="/Login">Log in to your account</Link></p>
+
+        {error && <p className="error">{error}</p>}
 
         <form onSubmit={formik.handleSubmit}>
           <div className='signininputs'>
             <label>First Name</label>
-            <input 
-              type="text" 
-              placeholder='Your first name' 
-              name='firstname' 
-              value={formik.values.firstname} 
-              onChange={formik.handleChange} 
-              onBlur={formik.handleBlur} 
+            <input
+              type="text"
+              placeholder='Your first name'
+              name='firstName'
+              value={formik.values.firstName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               required
             />
-            {formik.touched.firstname && formik.errors.firstname && <p>{formik.errors.firstname}</p>}
+            {formik.touched.firstName && formik.errors.firstName && <p>{formik.errors.firstName}</p>}
           </div>
 
           <div className='signininputs'>
             <label>Last Name</label>
-            <input 
-              type="text" 
-              placeholder='Your last name' 
-              name='lastname' 
-              value={formik.values.lastname} 
-              onChange={formik.handleChange} 
-              onBlur={formik.handleBlur} 
+            <input
+              type="text"
+              placeholder='Your last name'
+              name='lastName'
+              value={formik.values.lastName}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               required
             />
-            {formik.touched.lastname && formik.errors.lastname && <p>{formik.errors.lastname}</p>}
+            {formik.touched.lastName && formik.errors.lastName && <p>{formik.errors.lastName}</p>}
           </div>
 
           <div className='signininputs'>
             <label>Email Address</label>
-            <input 
-              type="email" 
-              placeholder='Your email' 
-              name='email' 
-              value={formik.values.email} 
-              onChange={formik.handleChange} 
-              onBlur={formik.handleBlur} 
+            <input
+              type="email"
+              placeholder='Your email'
+              name='email'
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
               required
             />
             {formik.touched.email && formik.errors.email && <p>{formik.errors.email}</p>}
@@ -115,19 +103,31 @@ function Signin() {
 
           <div className='signininputs'>
             <label>Password</label>
-            <input 
-              type="password" 
-              placeholder='Create a strong password' 
-              name='password' 
-              value={formik.values.password} 
+            <input
+              type="password"
+              placeholder='Create a strong password'
+              name='password'
+              value={formik.values.password}
               onChange={formik.handleChange}
-              onBlur={formik.handleBlur} 
+              onBlur={formik.handleBlur}
               required
             />
             {formik.touched.password && formik.errors.password && <p>{formik.errors.password}</p>}
           </div>
 
-        
+          <div className='signininputs'>
+            <label>Confirm Password</label>
+            <input
+              type="password"
+              placeholder='Confirm your password'
+              name='confirmPassword'
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
+            />
+            {formik.touched.confirmPassword && formik.errors.confirmPassword && <p>{formik.errors.confirmPassword}</p>}
+          </div>
 
           <button type='submit' className='createaccountbtn' disabled={loading}>
             {loading ? 'Please wait...' : "Submit"}
@@ -135,10 +135,9 @@ function Signin() {
 
           <div className='alternativesignin'>
             <p>Or sign in with</p>
-
-            <button className='googlesignin'><FcGoogle/> Google</button>
+            <button className='googlesignin'><FcGoogle /> Google</button>
             <button className='googlesignin'><FaFacebookMessenger /> Messenger</button>
-            <button className='googlesignin'><FaGithub/> Github</button>
+            <button className='googlesignin'><FaGithub /> Github</button>
           </div>
         </form>
       </div>
@@ -148,10 +147,10 @@ function Signin() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa accusamus amet iste sunt minus ut.</p>
         <button className='googlesignin'><FaFacebookMessenger /> Messenger</button>
-        <button className='googlesignin'><FaGithub/> Github</button>
+        <button className='googlesignin'><FaGithub /> Github</button>
       </div>
     </section>
-  )
+  );
 }
 
 export default Signin;
