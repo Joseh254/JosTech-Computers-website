a79697a2eea384a6f8dc4bf11c9ee25c2ad13fa5
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-08-28 18:35:42 +0300

    -

--- client/src/pages/Hotdeals/Hotdeals.jsx
+++ client/src/pages/Hotdeals/Hotdeals.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Hotdeals() {
-  return <div>Hotdeals</div>;
+  return (
+    <div className="hotdealsSection">
+      <div>
+        
+      </div>
+
+    </div>
+  );
 }
 
 export default Hotdeals;
