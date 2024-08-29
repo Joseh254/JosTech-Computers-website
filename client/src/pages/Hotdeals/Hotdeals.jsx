9ec0b774949d7a6e05f7614b461a6e8368e8cfee
Joseph Mbugua <josephkarimambugua@gmail.com>
2024-08-29 13:41:46 +0300

    -

--- client/src/pages/Hotdeals/Hotdeals.jsx
+++ client/src/pages/Hotdeals/Hotdeals.jsx
@@ -4,7 +4,8 @@ function Hotdeals() {
   return (
     <div className="hotdealsSection">
       <div>
-        
+        <h1>Get Our New offers</h1>
+        <h1>subscribe to our chanel</h1>
       </div>
 
     </div>
